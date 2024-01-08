68efe44f13d936e67389bd4394078ef31008efc4
Basil Saji <basilsaji@GMG.LOCAL>
2024-01-09 00:23:41 +0530

    Itaskrepo

---
+++ TaskMgmt.DataAccess/Repositories/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMgmt.DataAccess.Models;
+
+namespace TaskMgmt.DataAccess.Repositories
+{
+    public interface ITaskRepository
+    {
+        public Task<ProjectTask[]> GetAll();
+        public Task<ProjectTask> GetById();
+        public Task<ProjectTask> Add(ProjectTask task);
+
+    }
+}
