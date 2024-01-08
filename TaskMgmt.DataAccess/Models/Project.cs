832278c90001c5d607df0f297b0292b967da2ef0
viswa <65163117+viswa@users.noreply.github.com>
2024-01-08 19:50:09 +0530

    Add task models

--- TaskMgmt.DataAccess/Models/Project.cs
+++ TaskMgmt.DataAccess/Models/Project.cs
@@ -6,7 +6,7 @@ namespace TaskMgmt.DataAccess.Models
     public partial class Project
     {
         public int ProjectId { get; set; }
-        public int? GroupId { get; set; }
+        public int GroupId { get; set; }
         public string ProjectName { get; set; } = null!;
         public string ProjectDescription { get; set; } = null!;
         public int OwnerId { get; set; }
---
+++ TaskMgmt.DataAccess/Models/ProjectTask.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskMgmt.DataAccess.Models
+{
+    public class ProjectTask
+    {
+        public int TaskId { get; set; }
+        public string Description { get; set; } = null!;
+        public int ProjectId { get; set; }
+        public int StatusId { get; set; }
+
+        public TaskStatus TaskStatus { get; set; } = null!;
+        public Project Project { get; set; } = null!;
+    }
+}
