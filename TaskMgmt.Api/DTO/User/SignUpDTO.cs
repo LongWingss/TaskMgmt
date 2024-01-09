ab400ced7e3f10898df66e4608bc35b62c8af540
Aravind K Subash <aravindks@GMG.LOCAL>
2024-01-09 15:46:01 +0530

    Change swagger congiguration , Add Validation

--- TaskMgmt.Api/DTO/User/SignUpDTO.cs
+++ TaskMgmt.Api/DTO/User/SignUpDTO.cs
@@ -1,10 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskMgmt.Api.DTO
 {
     public class SignUpDTO
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, ErrorMessage = "Password must be between 6-20 characters ", MinimumLength = 6)]
+
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name must be less than 100 characters.", MinimumLength = 1)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "GroupName is required.")]
+        [StringLength(100, ErrorMessage = "GroupName must be less than 100 characters.", MinimumLength = 1)]
         public string GroupName { get; set; }
     }
 }
