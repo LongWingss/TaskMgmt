b38b00fec0fa11e42632e30e6292a2559a48d7b1
Ashish Binu <ashishbinu90@gmail.com>
2024-01-09 13:51:02 +0530

    update dto folder

--- TaskMgmt.Api/Controllers/ProjectController.cs
+++ TaskMgmt.Api/Controllers/ProjectController.cs
@@ -1,7 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using TaskMgmt.DataAccess.Dtos;
+using TaskMgmt.Api.Dtos;
 using TaskMgmt.DataAccess.Models;
 using TaskMgmt.DataAccess.Repositories;
 
