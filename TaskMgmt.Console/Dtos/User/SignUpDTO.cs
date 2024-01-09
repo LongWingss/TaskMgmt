9194c3f8692dce0e654eb01841593cf3c60122dc
Dheeraj B <dheerajb@cet.ac.in>
2024-01-09 18:43:33 +0530

    Console for Login and SignUp implemented Navneeth Mass Implemented Service for HttpClient

---
+++ TaskMgmt.Console/Dtos/User/SignUpDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskMgmt.Console.Dtos.User
+{
+    public class SignUpDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+
+    }
+}
--- TaskMgmt.Console/Program.cs
+++ TaskMgmt.Console/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TaskMgmt.Console;
+static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to the Task Management Console App!");
+        Menu menu = new Menu();
+        while (true)
+        {
+            Console.WriteLine("\n1. Sign In");
+            Console.WriteLine("2. Sign Up");
+            Console.WriteLine("3. Sign Up with Referral");
+            Console.WriteLine("4. Exit");
+
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    var signInTask = menu.SignIn();
+                    if(signInTask.Result)
+                    {
+                        Console.WriteLine("Success");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sign-in failed. Please try again.");
+                    }
+                    break;
+                case "2":
+                    menu.SignUp();
+                    break;
+                case "3":
+                    menu.SignUpReferral();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+
+
+
+
+}
