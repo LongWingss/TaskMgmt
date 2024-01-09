cc7df796db359d7b269f8c087001560ef302a4fe
Benno Baby <bennob@carestack.com>
2024-01-09 12:03:17 +0530

    implemented project task service

--- TaskMgmt.Services/DTOs/NewTask.cs
+++ TaskMgmt.Services/DTOs/NewTask.cs
@@ -2,5 +2,15 @@
 {
     public class NewTask
     {
+        
+        public string Description { get; set; } 
+        public DateTime DueDate { get; set; } 
+        public string CreatedBy { get; set; } 
+        public string CurrentStatus { get; set; } 
+
+        public int AssigneeId { get; set; }
+        public int CreatorId { get; set; }
+        public int ProjectId { get; set; }
+        public int CurrentStatusId { get; set; }
     }
 }
--- TaskMgmt.Services/DTOs/ProjectTask.cs
+++ TaskMgmt.Services/DTOs/ProjectTask.cs
@@ -2,11 +2,6 @@
 {
     public class ProjectTask
     {
-        public int TaskId { get; set; }
-        public string Description { get; set; } = null!;
-        public string DueDate { get; set; } = null!;
-        public string Assignee { get; set; } = null!;
-        public string CreatedBy { get; set; } = null!;
-        public string CurrentStatus { get; set; } = null!;
+        
     }
 }
