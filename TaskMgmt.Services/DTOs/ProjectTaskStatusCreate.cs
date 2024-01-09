52f261a0663b4f3ddcc809e55510bb265e524610
Basil Saji <basilsaji222@gmail.com>
2024-01-09 12:31:14 +0530

    implementing ProjectTaskStatusServices

---
+++ TaskMgmt.Services/DTOs/ProjectTaskStatusCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMgmt.DataAccess.Models;
+
+namespace TaskMgmt.Services.DTOs
+{
+    public class ProjectTaskStatusCreate
+    {
+
+        public int ProjectId { get; set; }
+        public string StatusText { get; set; } = null!;
+        public string StatusColor { get; set; } = null!;
+
+    }
+}
