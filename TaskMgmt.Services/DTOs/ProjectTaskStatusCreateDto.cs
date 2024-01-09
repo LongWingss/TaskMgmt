6e18e64a3c91d79a99a227d0ea9a02cfe295dc2c
viswa <65163117+viswa@users.noreply.github.com>
2024-01-09 20:01:01 +0530

    Allow only caps for color

--- TaskMgmt.Services/DTOs/ProjectTaskStatusCreateDto.cs
+++ TaskMgmt.Services/DTOs/ProjectTaskStatusCreateDto.cs
@@ -12,7 +12,7 @@ namespace TaskMgmt.Services.DTOs
         public int ProjectId { get; set; }
         [MaxLength(20, ErrorMessage = "Status text must be less than 20")]
         public string StatusText { get; set; } = null!;
-        [RegularExpression("^#([0-9A-Fa-f]{6})$", ErrorMessage = "Invalid hexadecimal color format")]
+        [RegularExpression("^#([0-9A-F]{6})$", ErrorMessage = "Invalid hexadecimal color format")]
         public string StatusColor { get; set; } = null!;
     }
 }
