61b30ea03ca0a84c2fd1409f0595f92600228eee
Benno Baby <bennob@carestack.com>
2024-01-09 12:40:29 +0530

    implemented project task service

--- TaskMgmt.Services/DTOs/NewTask.cs
+++ TaskMgmt.Services/DTOs/NewTask.cs
@@ -2,11 +2,11 @@
 {
     public class NewTask
     {
-        
-        public string Description { get; set; } 
+
+        public string Description { get; set; } = null!;
         public DateTime DueDate { get; set; } 
-        public string CreatedBy { get; set; } 
-        public string CurrentStatus { get; set; } 
+        public string CreatedBy { get; set; } = null!;
+        public string CurrentStatus { get; set; } = null!;
 
         public int AssigneeId { get; set; }
         public int CreatorId { get; set; }
--- TaskMgmt.Services/ProjectTasks/ProjectTaskService.cs
+++ TaskMgmt.Services/ProjectTasks/ProjectTaskService.cs
@@ -1,10 +1,23 @@
 ﻿using TaskMgmt.DataAccess.Models;
+using TaskMgmt.DataAccess.Repositories;
 using TaskMgmt.Services.DTOs;
 
 namespace TaskMgmt.Services.ProjectTasks
 {
     public class ProjectTaskService : IProjectTaskService
+
     {
+
+        private readonly ITaskRepository _taskRepository;
+
+        private readonly IProjectTaskStatusRepository _projectTaskStatusRepository;
+
+        public ProjectTaskService(ITaskRepository taskRepository, IProjectTaskStatusRepository projectTaskStatusRepository)
+        {
+            _taskRepository = taskRepository;
+            _projectTaskStatusRepository = projectTaskStatusRepository;
+        }
+
         public IEnumerable<DataAccess.DTOs.ProjectTask> GetAll(int projectId)
         {
             throw new NotImplementedException();
@@ -15,9 +28,24 @@ namespace TaskMgmt.Services.ProjectTasks
             throw new NotImplementedException();
         }
 
-        public void CreateTask(NewTask newTask)
+        public async void CreateTask(NewTask newTask)
         {
-            throw new NotImplementedException();
+            var task = new ProjectTask
+            {
+                Description = newTask.Description,
+                DueDate = newTask.DueDate,
+                AssigneeId = newTask.AssigneeId,
+                CreatorId = newTask.CreatorId,
+                ProjectId = newTask.ProjectId,
+                CurrentStatusId = newTask.CurrentStatusId
+
+
+
+            };
+
+            await _taskRepository.Add(task);
+
+
         }
     }
 }
