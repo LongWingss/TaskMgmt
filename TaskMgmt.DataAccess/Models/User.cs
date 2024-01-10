0bbc62b40205dccaa7e16fb82b9faa6e2053085e
Ashish Binu <ashishbinu90@gmail.com>
2024-01-10 11:57:33 +0530

    fix: add username on User

--- TaskMgmt.DataAccess/Models/User.cs
+++ TaskMgmt.DataAccess/Models/User.cs
@@ -7,6 +7,7 @@ namespace TaskMgmt.DataAccess.Models
     public class User
     {
         public int UserId { get; set; }
+        public string Username {get; set;} = null!;
         public string Email { get; set; } = null!;
         public string PasswordHash { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
