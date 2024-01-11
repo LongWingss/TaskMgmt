d1a1915344f2996cf382dc3df55f2257f02d997a
Dheeraj B <dheerajb@cet.ac.in>
2024-01-11 11:13:21 +0530

    signup with referral and signup (bug)

--- TaskMgmt.Console/Program.cs
+++ TaskMgmt.Console/Program.cs
@@ -85,15 +85,100 @@ static class Program
                         userService.SignUp();
                         if (userService.IsUserValid())
                         {
-                            Console.WriteLine("Account Created Succesfully!");   
+                            Console.WriteLine("Signup Success!");
                             groupService.DisplayGroup(userService.db.token);
-                            groupService.DisplayMenu();
+                            string response = groupService.DisplayMenu();
+                            switch (response)
+                            {
+                                case "1":
+                                    userService.db.groupId = projectService.GetProjects(userService.db.token);
+                                    var res = projectService.ProjectMenu(userService.db.token, userService.db.groupId);
+                                    switch (res)
+                                    {
+                                        case "1":
+                                            projectService.InviteUser(userService.db.token, userService.db.groupId);
+                                            break;
+                                        case "2":
+                                            var flag = projectService.CreateNewProject(userService.db.token, userService.db.groupId);
+
+                                            break;
+                                        case "3":
+                                            System.Console.WriteLine("Project ID: ");
+                                            var projectId = Convert.ToInt32(Console.ReadLine() ?? "0");
+                                            statusService.GetTasks(userService.db.token, userService.db.groupId, projectId);
+                                            taskService.GetTasks(userService.db.token, userService.db.groupId, projectId);
+                                            System.Console.WriteLine("1. Create New Task\n2. Go Back");
+                                            switch (Console.ReadLine() ?? string.Empty)
+                                            {
+                                                case "1":
+                                                    taskService.CreateTask(userService.db.token, userService.db.groupId, projectId);
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    break;
+                                case "2":
+                                    groupService.Enroll(userService.db.token);
+                                    break;
+                                case "3":
+                                    groupService.CreateGroup(userService.db.token);
+                                    break;
+                            }
                         }
-                        Console.ReadLine();
 
                         break;
                     case "3":
-                        
+                        userService.SignUpReferral();
+                        if (userService.IsUserValid())
+                        {
+                            Console.WriteLine("Referral Success!");
+                            groupService.DisplayGroup(userService.db.token);
+                            string response = groupService.DisplayMenu();
+                            switch (response)
+                            {
+                                case "1":
+                                    userService.db.groupId = projectService.GetProjects(userService.db.token);
+                                    var res = projectService.ProjectMenu(userService.db.token, userService.db.groupId);
+                                    switch (res)
+                                    {
+                                        case "1":
+                                            projectService.InviteUser(userService.db.token, userService.db.groupId);
+                                            break;
+                                        case "2":
+                                            var flag = projectService.CreateNewProject(userService.db.token, userService.db.groupId);
+
+                                            break;
+                                        case "3":
+                                            System.Console.WriteLine("Project ID: ");
+                                            var projectId = Convert.ToInt32(Console.ReadLine() ?? "0");
+                                            statusService.GetTasks(userService.db.token, userService.db.groupId, projectId);
+                                            taskService.GetTasks(userService.db.token, userService.db.groupId, projectId);
+                                            System.Console.WriteLine("1. Create New Task\n2. Go Back");
+                                            switch (Console.ReadLine() ?? string.Empty)
+                                            {
+                                                case "1":
+                                                    taskService.CreateTask(userService.db.token, userService.db.groupId, projectId);
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    break;
+                                case "2":
+                                    groupService.Enroll(userService.db.token);
+                                    break;
+                                case "3":
+                                    groupService.CreateGroup(userService.db.token);
+                                    break;
+                            }
+                        }
                         break;
                     case "4":
                         Environment.Exit(0);
